7310f0a677438cb0e00ed0e94fbafb4631324a8e
Luke Jones <luke@lukeojones.com>
2023-03-21 08:59:28 +0000

    feat(kvs) Extract Cargo Authors via Env Var

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,45 @@
+use clap::{Args, Parser, Subcommand, ValueEnum};
+
 fn main() {
-    println!("Hello, world!");
+    let args = KvArgs::parse();
+    println!("{:?}", args);
+
+    let authors = env!("CARGO_PKG_AUTHORS");
+    println!("{:?}", authors);
+}
+
+/// Reads and Analyses Files
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct KvArgs {
+    /// Operation to perform on KV
+    #[clap(subcommand)]
+    pub operation: Operation,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Operation {
+    /// Get a value by key
+    Get(KeyOnlyCommand),
+
+    /// Set a value by key
+    Set(KeyAndValueCommand),
+
+    /// Remove a value by key
+    #[clap(name = "rm")]
+    Remove(KeyOnlyCommand),
+}
+
+#[derive(Args, Debug)]
+pub struct KeyOnlyCommand {
+    /// Name of key to get value for
+    key: String,
+}
+
+#[derive(Args, Debug)]
+pub struct KeyAndValueCommand {
+    /// Name of key to get value for
+    key: String,
+    /// Value to set for key
+    value: String,
 }
